f771cf3482ba70ae34075aa37233035cf2b4c3c4
Quantyy <147756922+DR099H@users.noreply.github.com>
2024-06-18 12:24:26 +0300

    Add files

---
+++ panic-button/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "bodacious"
+games { 'gta5' }
+
+author "Quantyy"
+description "Panic Button Addon for FivePD (FivePD is the best!)"
+version "1.0.1"
+
+server_script "Panic_Server.net.dll"
+
+files {
+    "SharpConfig.dll",
+    "config.ini"
+}
